950c46e9b2e630de239220f4529085df25989ba8
lShamanl <sanyaerin@mail.ru>
2020-04-20 20:19:59 +0300

    refactoring DataGuard.php

--- src/Classes/DataGuard.php
+++ src/Classes/DataGuard.php
@@ -6,19 +6,49 @@ namespace lShamanl\ApiManager\Classes;
 use lShamanl\ApiAnswer\StatusCode;
 use lShamanl\ApiManager\Exceptions\InvalidDataException;
 
-
+/**
+ * Class DataGuard
+ * @package lShamanl\ApiManager\Classes
+ */
 class DataGuard
 {
     /**
      * @param array $params
+     * @param bool $strict
      * @return void
      * @throws InvalidDataException
      */
-    public static function required(array $params)
+    public static function required(array $params, $strict = true)
+    {
+        if ($strict) {
+            self::strictGuard($params);
+        } else {
+            self::notStrictGuard($params);
+        }
+    }
+
+    /**
+     * @param array $params
+     * @throws InvalidDataException
+     */
+    protected static function strictGuard(array $params)
+    {
+        foreach ($params as $key => $param) {
+            if (empty($param)) {
+                throw new InvalidDataException("Обязательное поле '{$key}' не было получено",StatusCode::HTTP_BAD_REQUEST);
+            }
+        }
+    }
+
+    /**
+     * @param array $params
+     * @throws InvalidDataException
+     */
+    protected static function notStrictGuard(array $params)
     {
         foreach ($params as $key => $param) {
             if (!isset($param)) {
-                throw new InvalidDataException("Обязательное поле '{$key}' не было получено",StatusCode::HTTP_BAD_REQUEST);
+                throw new InvalidDataException("Обязательное поле '{$key}' не было получено", StatusCode::HTTP_BAD_REQUEST);
             }
         }
     }
